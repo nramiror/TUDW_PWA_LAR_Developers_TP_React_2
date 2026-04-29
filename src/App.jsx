d52733f3b1177007bfb45ea58dcf61b4c8c4e81f
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2026-04-29 19:41:54 -0300

    - Creation of header component and inclusion of logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
 
   return (
     <div className="flex min-h-screen flex-col bg-gray-100">
+      <Header />
       <main className="flex flex-1 items-center justify-center">
         <h1 className="text-4xl font-bold text-blue-600">¡Tailwind está vivo! 🎲</h1>
       </main>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full h-20 px-8 flex items-center justify-between border-b border-[#829AB1]/30 bg-[linear-gradient(to_left,#E2F3FF_0%,#FFFFFF_100%)] shadow-[0_1px_12px_rgba(15,23,42,0.06)]">
+      
+      <div className="flex-shrink-0">
+        <img 
+          src="/LogoReactGamesCompleto.png" 
+          alt="ReactGames Logo" 
+          className="h-16 w-auto object-contain cursor-pointer md:h-20"
+        />
+      </div>
+
+      {/* CENTRO: SEARCH BOX */}
+      <div className="flex-grow max-w-md mx-8">
+        {/* Componente <SearchBox /> */}
+       
+      </div>
+
+      {/* DERECHA: ACCIONES*/}
+      <div className="flex items-center gap-6">
+        
+        {/* Nav de Favoritos */}
+        <nav className="hidden sm:block">
+          
+        </nav>
+
+        {/* Toggle de Idioma */}
+       
+
+      </div>
+
+    </header>
+  );
+};
+
+export default Header;
