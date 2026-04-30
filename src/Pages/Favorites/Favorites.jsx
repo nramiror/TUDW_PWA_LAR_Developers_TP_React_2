e4bf81d53154213469d2329bbfc9948aa1e11fc9
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2026-04-29 22:09:34 -0300

    Including basic content in pages to use the nav

---
+++ src/Pages/Favorites/Favorites.jsx
@@ -0,0 +1,10 @@
+const Favorites = () => {
+  return (
+    <div className="w-full">
+      <h1>Favoritos</h1>
+      {/* Contenido de favoritos aquí */}
+    </div>
+  );
+};
+
+export default Favorites;
