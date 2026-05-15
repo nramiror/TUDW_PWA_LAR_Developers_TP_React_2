22724dbe661c37ba80f6bb6b218d3a3b45f14627
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-05-15 19:19:42 -0300

    - Update sintax

--- src/Components/Title/title.test.jsx
+++ src/Components/Title/title.test.jsx
@@ -5,16 +5,14 @@ import {describe, it } from "vitest";
 import {render, screen} from "@testing-library/react";
 import Title from "./Title";
 
-describe("Title component H2", () => {
+describe("Title component", () => {
     it("renders the correct heading level based on the level prop", () => {
         render(<Title level={2} children="Test Heading" />);
         const heading = screen.getByText("Test Heading");
         expect(heading.tagName).toBe("H2");
     });
-});
 
-describe("Title component text", () => {
-    it("render correctly", () => {
+    it("render correctly text", () => {
         render(<Title level={3} children="Test Heading" />);
         const heading = screen.getByText("Test Heading");
         expect(heading).toBeInTheDocument();
