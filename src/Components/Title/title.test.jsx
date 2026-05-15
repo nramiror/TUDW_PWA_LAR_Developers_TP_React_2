2ecf264d92599b108d86504e33c571c6d31c8bf3
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-05-15 19:33:23 -0300

    - Add test for incorrect level and custom classname

--- src/Components/Title/title.test.jsx
+++ src/Components/Title/title.test.jsx
@@ -1,8 +1,8 @@
 //title muestra un texto que llega por props, el nivel del título se determina por la prop level, 
 // y se le pueden agregar clases adicionales con className
 
-import {describe, it } from "vitest";
-import {render, screen} from "@testing-library/react";
+import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
 import Title from "./Title";
 
 describe("Title component", () => {
@@ -17,4 +17,23 @@ describe("Title component", () => {
         const heading = screen.getByText("Test Heading");
         expect(heading).toBeInTheDocument();
     });
-}); 
+
+    it("error renders the incorrect heading level based on the level prop", () => {
+        render(<Title level={8} children="Test Heading" />);
+        const heading = screen.getByText("Test Heading");
+        expect(heading.tagName).toBe("H1");
+    });
+
+        it("error renders the incorrect heading level based on the level prop", () => {
+        render(<Title level={-2} children="Test Heading" />);
+        const heading = screen.getByText("Test Heading");
+        expect(heading.tagName).toBe("H1");
+    });
+
+        it("renders the correct heading with className prop", () => {
+        render(<Title level={8} children="Test Heading" className="customClass" />);
+        const customClass = "font-comfortaa font-bold text-title tracking-tight color-primary";
+        const heading = screen.getByText("Test Heading");
+        expect(heading).toHaveClass("customClass");
+    });
+});  
