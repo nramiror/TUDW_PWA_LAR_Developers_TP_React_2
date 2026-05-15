a764feae8a44e3b9417ac5e23eee011c7c532632
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-05-15 19:16:55 -0300

    - Add test in component title

---
+++ src/Components/Title/title.test.jsx
@@ -0,0 +1,22 @@
+//title muestra un texto que llega por props, el nivel del título se determina por la prop level, 
+// y se le pueden agregar clases adicionales con className
+
+import {describe, it } from "vitest";
+import {render, screen} from "@testing-library/react";
+import Title from "./Title";
+
+describe("Title component H2", () => {
+    it("renders the correct heading level based on the level prop", () => {
+        render(<Title level={2} children="Test Heading" />);
+        const heading = screen.getByText("Test Heading");
+        expect(heading.tagName).toBe("H2");
+    });
+});
+
+describe("Title component text", () => {
+    it("render correctly", () => {
+        render(<Title level={3} children="Test Heading" />);
+        const heading = screen.getByText("Test Heading");
+        expect(heading).toBeInTheDocument();
+    });
+}); 
