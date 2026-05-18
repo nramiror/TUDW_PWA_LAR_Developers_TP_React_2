d13be7431ef5aec43ccfa25f60419f9798ced81b
Linda Parra <linda.parra@est.fi.uncoma.edu.ar>
2026-05-18 19:08:29 -0300

    - Add config coverage and script

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,17 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/utils/setupTests.js',
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'src/main.jsx',
+        'src/i18n.js',
+        'src/utils/setupTests.js',
+        '**/*.test.*',
+        '**/*.spec.*',
+      ],
+    },
   },
 })
