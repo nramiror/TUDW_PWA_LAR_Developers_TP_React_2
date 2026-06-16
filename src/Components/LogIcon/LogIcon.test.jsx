0e2afc12c3dbef4081ea9f1d638a49ec166cb113
Ramir0Navarrete <39681359rr@gmail.com>
2026-06-16 19:18:26 -0300

    - Add test in logIcon component

---
+++ src/Components/LogIcon/LogIcon.test.jsx
@@ -0,0 +1,21 @@
+import { describe, it, vi, expect } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import LoginIcon from "./LoginIcon"; 
+
+describe("LoginIcon component", () => {
+    it("renders correctly as a button", () => {
+        render(<LoginIcon onClick={() => {}} />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+
+    it("triggers the onClick prop when clicked", () => {
+        const onClickMock = vi.fn();
+        render(<LoginIcon onClick={onClickMock} />);
+        
+        const button = screen.getByRole("button");
+        fireEvent.click(button);
+        
+        expect(onClickMock).toHaveBeenCalledTimes(1);
+    });
+});
